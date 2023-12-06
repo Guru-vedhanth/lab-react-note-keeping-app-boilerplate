b4f484235e620d41342a53b6ac4ea6a9282d537b
GURUVEDHANTH S <officialvedhanth@gmail.com>
2023-12-06 11:05:29 +0530

    update

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Editor from './component/Editor'
+
+export class App extends Component {
+  render() {
+    return (
+      <Editor/>
+    )
+  }
+}
+
+export default App
